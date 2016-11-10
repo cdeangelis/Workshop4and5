6b6a7382cba1b70e6055f3477ba639758c24dfe0
cdeangelis <cdeangelis@umass.edu>
2016-11-10 16:37:21 -0500

    fb6

--- app/components/feed.js
+++ app/components/feed.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FeedItem from './feeditem';
 import StatusUpdateEntry from './statusupdateentry';
-import {getFeedData} from '../server';
+import {getFeedData, postStatusUpdate} from '../server';
 
 export default class Feed extends React.Component {
 	constructor(props) {
@@ -20,18 +20,23 @@ export default class Feed extends React.Component {
 			contents: []
 		};
     }
-    render() {
-        return (
-            <div>
-              <StatusUpdateEntry />
-              {this.state.contents.map((feedItem) => {
-                return (
-                  <FeedItem key={feedItem._id} data={feedItem} />
-                );
-              })}
-            </div>
-        )
+
+    refresh() {
+        getFeedData(this.props.user, (feedData) => {
+            this.setState(feedData);
+        });
     }
+
+    onPost(postContents) {
+        // Send to server.
+        // We could use geolocation to get a location,
+        // but let's fix it to Amherst for now.
+        postStatusUpdate(4, "Amherst, MA", postContents, () => {
+            // Database is now updated. Refresh the feed.
+            this.refresh();
+        });
+    }
+
     componentDidMount() {
         getFeedData(this.props.user, (feedData) => {
             // Note: setState does a *shallow merge* of
@@ -41,5 +46,18 @@ export default class Feed extends React.Component {
             // {foo: 3, bar: 5}. This won't be a problem here.
             this.setState(feedData);
         });
+    }
+
+    render() {
+        return (
+          <div>
+            <StatusUpdateEntry onPost={(postContents) => this.onPost(postContents)} />
+            {this.state.contents.map((feedItem) => {
+            return (
+              <FeedItem key={feedItem._id} data={feedItem} />
+            )
+            })}
+          </div>
+        )
     }
 }
--- app/components/statusupdateentry.js
+++ app/components/statusupdateentry.js
@@ -1,22 +1,61 @@
 import React from 'react';
 
+
 export default class StatusUpdateEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+  /**
+  * Called when the user clicks the 'post' button.
+  * Triggers the `onPost` prop if the post isn't empty, and clears
+  * the component.
+  */
+  handlePost(e) {
+    // Prevent the event from "bubbling" up the DOM tree.
+    e.preventDefault();
+    // Trim whitespace from beginning + end of entry.
+    var statusUpdateText = this.state.value.trim();
+    if (statusUpdateText !== "") {
+      this.props.onPost(statusUpdateText)
+      // Reset status update.
+      this.setState({value: ""});
+    }
+  }
+  /**
+  * Called when the user types a character into the status update box.
+  * @param e An Event object.
+  */
+  handleChange(e) {
+    // Prevent the event from "bubbling" up the DOM tree.
+    e.preventDefault();
+    // e.target is the React Virtual DOM target of the
+    // input event -- the <textarea> element. The textarea's
+    // `value` is the entire contents of what the user has
+    // typed in so far.
+    this.setState({value: e.target.value});
+  }
   render() {
     return (
       <div className="fb-status-update-entry panel panel-default">
         <div className="panel-body">
           <ul className="nav nav-pills">
             <li role="presentation" className="active">
-              <a href="#"><span className="glyphicon glyphicon-pencil">
-              </span> <strong>Update Status</strong></a>
+              <a href="#">
+                <span className="glyphicon glyphicon-pencil">
+                </span> <strong>Update Status</strong></a>
             </li>
             <li role="presentation">
-              <a href="#"><span className="glyphicon glyphicon-picture">
-              </span> <strong>Add Photos/Video</strong></a>
+              <a href="#">
+                <span className="glyphicon glyphicon-picture">
+                </span> <strong>Add Photos/Video</strong></a>
             </li>
             <li role="presentation">
-              <a href="#"><span className="glyphicon glyphicon-th">
-              </span> <strong>Create Photo Album</strong></a>
+              <a href="#">
+                <span className="glyphicon glyphicon-th">
+                </span> <strong>Create Photo Album</strong></a>
             </li>
           </ul>
           <div className="media">
@@ -25,8 +64,11 @@ export default class StatusUpdateEntry extends React.Component {
             </div>
             <div className="media-body">
               <div className="form-group">
-                <textarea className="form-control" rows="2" placeholder="What's on your mind?">
-                </textarea>
+                <textarea className="form-control"
+                          rows="2"
+                          placeholder="What's on your mind?"
+                          value={this.state.value}
+                          onChange={(e) => this.handleChange(e)} />
               </div>
             </div>
           </div>
@@ -40,7 +82,7 @@ export default class StatusUpdateEntry extends React.Component {
                   <span className="glyphicon glyphicon-user"></span>
                 </button>
                 <button type="button" className="btn btn-default">
-                  ☺
+                  <span className="glyphicon glyphicon-heart"></span>
                 </button>
                 <button type="button" className="btn btn-default">
                   <span className="glyphicon glyphicon-pushpin"></span>
@@ -54,7 +96,7 @@ export default class StatusUpdateEntry extends React.Component {
                   Friends
                   <span className="caret"></span>
                 </button>
-                <button type="button" className="btn btn-default">
+                <button type="button" className="btn btn-default" onClick={(e) => this.handlePost(e)}>
                   Post
                 </button>
               </div>
